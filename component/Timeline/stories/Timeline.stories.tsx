20afd7e9d48e91685118d3654abb77f0adec08d3
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-08-12 12:12:37 +0900

    chore: fix Timeline storybook

--- component/Timeline/stories/Timeline.stories.tsx
+++ component/Timeline/stories/Timeline.stories.tsx
@@ -1,14 +1,29 @@
-import {Timeline as TimelineComponent} from "../index";
-import type { Meta, StoryObj } from '@storybook/react';
+import { Timeline as TimelineComponent } from '../index'
+import type { Meta, StoryObj } from '@storybook/react'
 
 const meta: Meta<typeof TimelineComponent> = {
-    component: TimelineComponent,
-    title: 'Timeline',
-};
+  component: TimelineComponent,
+  title: 'Timeline',
+}
 
-export default meta;
-type Story = StoryObj<typeof TimelineComponent>;
+export default meta
+type Story = StoryObj<typeof TimelineComponent>
+
+const items = [
+  {
+    label: '大宮',
+    color: 'primary',
+  },
+  {
+    label: '新潟',
+    color: 'primary',
+  },
+  {
+    label: 'スキー場',
+    color: 'primary',
+  },
+]
 
 export const Timeline: Story = {
-    render: () => <TimelineComponent />,
-};
+  render: () => <TimelineComponent items={items} />,
+}
