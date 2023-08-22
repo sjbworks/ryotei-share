f9fb1b858a395476b9d5321367e61d5b0434e199
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-08-23 00:14:56 +0900

    chore: TimelineItem style

--- component/Timeline/TimelineItem.tsx
+++ component/Timeline/TimelineItem.tsx
@@ -10,6 +10,7 @@ import { Typography } from '@mui/material'
 import { FC } from 'react'
 
 export type TimelineItemProps = {
+  time: string
   label: string
   color: string
   isLast?: boolean
@@ -17,11 +18,24 @@ export type TimelineItemProps = {
 }
 
 export const TimelineItem: FC<TimelineItemProps> = (props) => {
-  const { label, color, isLast, title } = props
+  const { time, label, color, isLast, title } = props
   return (
     <MUITimelineItem>
-      <TimelineOppositeContent sx={{ m: 'auto 0' }} align="right" variant="body2" color="text.secondary">
-        9:30 am
+      {/*<TimelineOppositeContent sx={{ m: 'auto 0' }} align="right" variant="body2" color="text.secondary">*/}
+      <TimelineOppositeContent
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          px: 2,
+          justifyContent: 'center',
+        }}
+      >
+        {title && (
+          <Typography variant="h6" component="span">
+            {title}
+          </Typography>
+        )}
+        {time}
       </TimelineOppositeContent>
       <TimelineSeparator>
         <TimelineConnector />
@@ -32,12 +46,11 @@ export const TimelineItem: FC<TimelineItemProps> = (props) => {
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          py: 'auto',
           px: 2,
-          alignItems: 'center',
           justifyContent: 'center',
         }}
       >
+        {/*<TimelineContent>*/}
         {title && (
           <Typography variant="h6" component="span">
             {title}
--- component/Timeline/stories/Timeline.stories.tsx
+++ component/Timeline/stories/Timeline.stories.tsx
@@ -11,15 +11,18 @@ type Story = StoryObj<typeof TimelineComponent>
 
 const items = [
   {
+    time: '9:00',
     label: '大宮',
     title: 'タイトル',
     color: 'primary',
   },
   {
+    time: '10:00',
     label: '新潟',
     color: 'primary',
   },
   {
+    time: '12:00',
     label: 'スキー場',
     color: 'primary',
     isLast: true,
