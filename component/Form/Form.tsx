1a70d06aa71806dcdbce3462539e8850d769173e
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-10-30 23:57:07 +0900

    create Form stories

--- component/Form/Form.tsx
+++ component/Form/Form.tsx
@@ -1,5 +1,7 @@
 import { useForm, Controller } from 'react-hook-form'
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
+import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import TextField from '@mui/material/TextField'
 
 export const Form = () => {
   const {
@@ -10,13 +12,21 @@ export const Form = () => {
     formState: { errors },
   } = useForm()
   return (
-    <div>
-      <Controller
-        name="datetime"
-        control={control}
-        rules={{ required: true }}
-        render={({ field }) => <DateTimePicker {...field} />}
-      ></Controller>
+    <div className="flex flex-col">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Controller
+          name="datetime"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <DateTimePicker {...field} />}
+        />
+      </LocalizationProvider>
+      <TextField
+        {...register('title', { required: true })}
+        error={!!errors.title}
+        helperText={errors.title && 'Title is required'}
+      />
+      <input type="submit" />
     </div>
   )
 }
---
+++ component/Form/stories/Form.stories.tsx
@@ -0,0 +1,17 @@
+import { Form as FormComponent } from '../Form'
+import type { Meta, StoryObj } from '@storybook/react'
+import { ComponentProps } from 'react'
+
+type Props = ComponentProps<typeof FormComponent>
+
+const meta: Meta<typeof FormComponent> = {
+  component: FormComponent,
+  title: 'Form',
+}
+
+export default meta
+type Story = StoryObj<typeof FormComponent>
+
+export const Timeline: Story = {
+  render: () => <FormComponent />,
+}
