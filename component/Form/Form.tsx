99d244cda67dc4461bfe2b2fa9d4ad6475dc37aa
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-11-01 23:54:21 +0900

    update Form

--- component/Form/Form.tsx
+++ component/Form/Form.tsx
@@ -2,6 +2,7 @@ import { useForm, Controller, SubmitHandler } from 'react-hook-form'
 import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box'
 import { Button } from '@/component/Button'
 
 type FormInput = {
@@ -13,13 +14,20 @@ export const Form = () => {
   const {
     register,
     handleSubmit,
-    watch,
     control,
     formState: { errors },
   } = useForm<FormInput>({ reValidateMode: 'onBlur', defaultValues: undefined })
   const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data)
   return (
-    <div className="grid gap-4">
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1 },
+      }}
+      noValidate
+      autoComplete="off"
+      className="flex flex-col"
+    >
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <Controller
           name="datetime"
@@ -37,6 +45,6 @@ export const Form = () => {
       <Button onClick={handleSubmit(onSubmit)} className="block w-full">
         送信
       </Button>
-    </div>
+    </Box>
   )
 }
