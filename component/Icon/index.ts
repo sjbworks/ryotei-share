899527fc46a5d47c1684afd9da6d2c81c2fa3b9f
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-11-27 01:07:10 +0900

    add AddIcon

--- component/Icon/index.ts
+++ component/Icon/index.ts
@@ -1,25 +1,4 @@
-// import FoodIcon from '@mui/icons-material/Restaurant'
-// import AirPlaneIcon from '@mui/icons-material/LocalAirportRounded'
-// import TrainIcon from '@mui/icons-material/DirectionsTransitRounded'
-// import TaxiIcon from '@mui/icons-material/LocalTaxiRounded'
-// import CarIcon from '@mui/icons-material/DirectionsCarRounded'
-// import WalkIcon from '@mui/icons-material/DirectionsWalkRounded'
-// import SleepIcon from '@mui/icons-material/HotelRounded'
-// import MorningIcon from '@mui/icons-material/LightModeRounded'
-// import MoonIcon from '@mui/icons-material/DarkModeRounded'
-// import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import AddIcon from '@mui/icons-material/Add'
 
-export {
-  // FoodIcon,
-  // AirPlaneIcon,
-  // TrainIcon,
-  // TaxiIcon,
-  // CarIcon,
-  // WalkIcon,
-  // SleepIcon,
-  // MorningIcon,
-  // MoonIcon,
-  // FiberManualRecordIcon,
-  AccessTimeIcon,
-}
+export { AccessTimeIcon, AddIcon }
