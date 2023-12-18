05218a6532104becea83147634b0b6174a7d4b6f
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-12-19 00:54:32 +0900

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 'use client'
-import { Timeline, BottomSheet, Layout, Form, Button, AddIcon, FormInput } from '@/component'
-import { useState, useMemo } from 'react'
+import { Timeline, BottomSheet, Layout, Form, Button, AddIcon, FormInput, TimeLineProps } from '@/component'
+import { useState, useMemo, MouseEvent } from 'react'
 import dayjs from 'dayjs'
 
 type Ryotei = Array<FormInput>
-type Props = { data: Record<string, Array<{ time: string; label: string }>> }
+type Props = { data: Record<string, TimeLineProps['items']> }
 
 const Timelines = ({ data }: Props) => {
   return (
@@ -17,37 +17,53 @@ const Timelines = ({ data }: Props) => {
 }
 
 export default function Home() {
-  const [open, setOpen] = useState(false)
-  const handleClick = () => setOpen(!open)
-  const onClose = () => setOpen(false)
-  const onOpen = () => setOpen(true)
-  const bottomSheet = { open, onOpen, onClose }
+  const [bottomOpen, setBottomOpen] = useState(false)
+  const handleClick = () => setBottomOpen(!bottomOpen)
+  const onBottomClose = () => setBottomOpen(false)
+  const onBottomOpen = () => setBottomOpen(true)
+  const bottomSheet = { open: bottomOpen, onOpen: onBottomOpen, onClose: onBottomClose }
   const containerStyle = 'flex flex-col justify-between p-10 max-w-xl m-auto container'
   const [data, setData] = useState<Ryotei>([])
 
-  const grouped: { [key: string]: { time: string; label: string }[] } = useMemo(() => {
-    const groupedData: { [key: string]: { time: string; label: string }[] } = data.reduce(
+  const [anchor, setAnchor] = useState<HTMLElement | null>(null)
+  const onClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchor(event.currentTarget)
+  }
+  const menuItems = [
+    { label: '削除', action: () => console.log('削除') },
+    { label: '編集', action: () => console.log('編集') },
+  ]
+  const onMenuClose = () => setAnchor(null)
+  const items = {
+    onClick,
+    onClose: onMenuClose,
+    anchor,
+    menuItems,
+  }
+
+  const grouped: { [key: string]: TimeLineProps['items'] } = useMemo(() => {
+    const groupedData: { [key: string]: TimeLineProps['items'] } = data.reduce(
       (acc, { datetime, description }) => {
         const date = dayjs(datetime).format('YYYY-MM-DD')
         if (date in acc) {
-          acc[date].push({ time: dayjs(datetime).format('HH:mm'), label: description })
+          acc[date].push({ time: dayjs(datetime).format('HH:mm'), label: description, ...items })
         } else {
-          acc[date] = [{ time: dayjs(datetime).format('HH:mm'), label: description }]
+          acc[date] = [{ time: dayjs(datetime).format('HH:mm'), label: description, ...items }]
         }
         return acc
       },
-      {} as { [key: string]: { time: string; label: string }[] }
+      {} as { [key: string]: TimeLineProps['items'] }
     )
 
     const sortedKeys = Object.keys(groupedData).sort() // キーを'YYYY-MM-DD'順にソート
 
-    const sortedGroupedData: { [key: string]: { time: string; label: string }[] } = {}
+    const sortedGroupedData: { [key: string]: TimeLineProps['items'] } = {}
     sortedKeys.forEach((key) => {
       sortedGroupedData[key] = groupedData[key]
     })
 
     return sortedGroupedData
-  }, [data])
+  }, [data]) // eslint-disable-line react-hooks/exhaustive-deps
 
   const setNewData = (newData: FormInput) => setData([...data, newData])
 
