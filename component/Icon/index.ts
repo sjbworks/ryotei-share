c6338caab16ca6f573740bde5de909a77418072f
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-12-20 00:25:18 +0900

    add EditIcon and DeleteIcon

--- component/Icon/index.ts
+++ component/Icon/index.ts
@@ -1,5 +1,7 @@
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
 import AddIcon from '@mui/icons-material/Add'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
 
-export { AccessTimeIcon, AddIcon, MoreHorizIcon }
+export { AccessTimeIcon, AddIcon, MoreHorizIcon, EditIcon, DeleteIcon }
