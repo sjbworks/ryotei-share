e5761d917408222a5ca6f51bf01ed3b9dee33d19
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-04-26 01:21:34 +0900

    fix import

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { AuthButton } from '@/component'
+import { AuthButton } from '@/feature/auth/components'
 
 export default function Login() {
   const containerStyle = 'flex flex-col justify-between p-10 max-w-xl m-auto container'
