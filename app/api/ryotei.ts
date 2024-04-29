e00d6a178f1eb7a79d039c4b1c8e4aa1ab2a90ec
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-04-30 00:33:49 +0900

    feat: add get and post method for ryotei

--- app/api/ryotei.ts
+++ app/api/ryotei.ts
@@ -1,14 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '@/utils/supabase/client'
+// import { supabase } from '@/utils/supabase/client'
 
 // Define your API routes here
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const userId = (await supabase.auth.getUser()).data.user?.id
   // Handle different API routes based on the request method
   if (req.method === 'GET') {
-    // Handle GET request
-    // Your code here
+    const { error } = await supabase.from('ryotei').select(`id, date, description`).eq('user_id', userId)
+    if (error) return res.status(401).json({ error: error.message })
   } else if (req.method === 'POST') {
-    // Handle POST request
-    // Your code here
+    const data = { ...req.body, user_id: userId }
+    const { error } = await supabase.from('ryotei').insert(data)
+    if (error) return res.status(401).json({ error: error.message })
   } else if (req.method === 'PUT') {
     // Handle PUT request
     // Your code here
