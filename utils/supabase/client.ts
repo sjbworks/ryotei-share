66ef4bd30e33bf449f23730f9ca2bcb472c99039
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-05-05 01:22:45 +0900

    feat: utils/supabase

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@/constants/supabase'
+import { createBrowserClient } from '@supabase/ssr'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export function createClient() {
+  return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+}
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,6 +1,5 @@
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@/constants/supabase'
 
 export const updateSession = async (request: NextRequest) => {
   let response = NextResponse.next({
@@ -9,47 +8,51 @@ export const updateSession = async (request: NextRequest) => {
     },
   })
 
-  const supabase = createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-    cookies: {
-      get(name) {
-        return request.cookies.get(name)?.value
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          })
+          response.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+        },
       },
-      set(name, value, options) {
-        request.cookies.set({
-          name,
-          value,
-          ...options,
-        })
-        response = NextResponse.next({
-          request: {
-            headers: request.headers,
-          },
-        })
-        response.cookies.set({
-          name,
-          value,
-          ...options,
-        })
-      },
-      remove(name, options) {
-        request.cookies.set({
-          name,
-          value: '',
-          ...options,
-        })
-        response = NextResponse.next({
-          request: {
-            headers: request.headers,
-          },
-        })
-        response.cookies.set({
-          name,
-          value: '',
-          ...options,
-        })
-      },
-    },
-  })
+    }
+  )
 
   // refreshing the auth token
   await supabase.auth.getUser()
