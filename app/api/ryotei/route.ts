40323a350505969c3bfec3458c7ff56405e21b9e
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-05-05 01:22:07 +0900

    fix: fix api/ryotei's POST process

--- app/api/ryotei/route.ts
+++ app/api/ryotei/route.ts
@@ -1,19 +1,23 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { supabase } from '@/utils/supabase/client'
-import { NextResponse } from 'next/server'
-import { format } from 'date-fns'
+import { createClient } from '@/utils/supabase/server'
+import { NextResponse, NextRequest } from 'next/server'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, _res: NextResponse) {
+  const supabase = createClient()
   const userId = (await supabase.auth.getUser()).data.user?.id
   const { error } = await supabase.from('ryotei').select(`id, date, description`).eq('user_id', userId)
   if (error) return NextResponse.json({ message: error.message })
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const userId = (await supabase.auth.getUser()).data.user?.id
-  console.log(userId)
-  const body = await new Response(req.body).json()
-  const data = { ...body, user_id: userId }
-  const { error } = await supabase.from('ryotei').insert(data)
-  if (error) return NextResponse.json({ message: error.message })
+export async function POST(req: NextRequest, _res: NextResponse) {
+  try {
+    const supabase = createClient()
+    const userId = (await supabase.auth.getUser()).data.user?.id
+    const body = await new Response(req.body).json()
+    const ryoteiData = { ...body, user_id: userId }
+    await supabase.from('ryotei').insert(ryoteiData)
+    return NextResponse.json({ success: true })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ message: error })
+  }
 }
