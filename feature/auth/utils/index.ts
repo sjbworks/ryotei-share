9d960567b5155931c22fab0ae801b4669fe5c702
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-05-05 01:23:25 +0900

    fix: fix feature/auth/utils

--- feature/auth/utils/index.ts
+++ feature/auth/utils/index.ts
@@ -1,28 +1,12 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@/utils/supabase/client'
 import { Provider } from '@supabase/gotrue-js'
 
-export const auth = () => {
-  const supabase = createClientComponentClient()
-
-  const signIn = async (provider: Provider) => {
-    try {
-      await supabase.auth.signInWithOAuth({
-        provider,
-        options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
-        },
-      })
-    } catch (error) {
-      // TODO: Handle error
-    }
-  }
-
-  const signOut = async () => {
-    try {
-      await supabase.auth.signOut()
-    } catch (error) {
-      // TODO: Handle error
-    }
-  }
-  return { signIn, signOut }
+export async function login(provider: Provider) {
+  const supabase = createClient()
+  await supabase.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo: `${window.location.origin}/api/auth/callback`,
+    },
+  })
 }
