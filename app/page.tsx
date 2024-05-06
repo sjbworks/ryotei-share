b26ff55c0f4bc6115dde2af1b1c908425c1dbd33
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-05-06 17:59:38 +0900

    chore: remove unnecessary code

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,15 @@
-'use client'
-import { Timeline, BottomSheet, Layout, Form, Button, AddIcon, TimeLineProps } from '@/component'
-import { useTimeline } from '@/feature/ryotei/hooks/useTimeline'
+import { cookies } from 'next/headers'
+import { TimelineView } from '@/feature/ryotei/components/TimelineView'
+import { getRyotei } from '@/feature/ryotei/api/getRyotei'
 
-type Props = { data: Record<string, TimeLineProps['items']>; onClick?: TimeLineProps['onClick'] }
-
-const Timelines = ({ data, onClick }: Props) => {
-  return (
-    <>
-      {Object.entries(data).map(([key, data]) => (
-        <Timeline key={key} title={key} items={data} onClick={onClick} className="mb-3" />
-      ))}
-    </>
-  )
-}
-
-export default function Home() {
+export default async function Home() {
   const containerStyle = 'flex flex-col justify-between p-10 max-w-xl m-auto container'
-  const { grouped, handleClick, bottomSheet, setNewData, onMenuClick } = useTimeline()
+  const res = await getRyotei(cookies().toString())
+  console.log(res)
 
   return (
     <main className={containerStyle}>
-      <Layout>
-        <Timelines data={grouped} onClick={onMenuClick} />
-        <Button variant="text" className="flex mt-4 items-center justify-items-center" onClick={handleClick}>
-          <AddIcon />
-          旅程を登録
-        </Button>
-        <BottomSheet {...bottomSheet}>
-          <Form className={containerStyle} setData={setNewData} />
-        </BottomSheet>
-      </Layout>
+      <TimelineView data={res} />
     </main>
   )
 }
