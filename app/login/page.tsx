fa4a30c63f7deaf9c5a3d11cfcaec88978e26c41
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-05-06 19:07:01 +0900

    fix: fix login path

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { AuthButton } from '@/feature/auth/components/AuthButton'
-import { login } from '@/feature/auth/utils'
+import { login } from '@/feature/auth/api'
 
 export default function Login() {
   const containerStyle = 'flex flex-col justify-between p-10 max-w-xl m-auto container'
