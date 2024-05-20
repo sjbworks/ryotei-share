fc512149c576c0a9e567c2848a1b7119c5c2810c
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-05-21 00:18:34 +0900

    chore: create share/[id]

---
+++ app/share/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { cookies } from 'next/headers'
+import { TimelineView } from '@/feature/ryotei/components/TimelineView'
+import { getRyotei } from '@/feature/ryotei/api'
+import { logout } from '@/feature/auth/api'
+import { Layout } from '@/component'
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params
+  const containerStyle = 'flex flex-col justify-between p-10 max-w-xl m-auto container'
+  const res = await getRyotei(cookies().toString())
+
+  return (
+    <main className={containerStyle}>
+      <Layout logout={logout}>
+        <TimelineView data={res} />
+      </Layout>
+    </main>
+  )
+}
