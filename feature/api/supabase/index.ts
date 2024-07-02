c758d8ebd93c7a0765ad3d6b39312412a8c325e9
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-07-03 00:25:31 +0900

    WIP: create gql file

---
+++ feature/api/supabase/index.ts
@@ -0,0 +1,42 @@
+import { createClient } from '@supabase/supabase-js'
+import { gql } from '@apollo/client'
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@/constants/supabase'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+/** 旅程一覧取得用クエリー */
+// export const getRyotei = gql(`
+//     query GetRyotei($orderBy: [tasksOrderBy!]) {
+//         ryoteiCollection(orderBy: $orderBy) {
+//             edges {
+//             node {
+//                 id
+//                 description
+//                 datetime
+//             }
+//             }
+//         }
+//     }
+// `)
+
+/** 新規タスク作成用ミューテーション */
+// export const taskInsert = gql(`
+//     mutation TaskMutation($objects: [tasksInsertInput!]!) {
+//         insertIntotasksCollection(objects: $objects) {
+//             records {
+//             title
+//             }
+//         }
+//     }
+// `)
+
+/** タスクのステータス更新用ミューテーション */
+// export const taskUpdate = gql(`
+//     mutation Mutation($set: tasksUpdateInput!, $filter: tasksFilter) {
+//         updatetasksCollection(set: $set, filter: $filter) {
+//             records {
+//                 is_completed
+//             }
+//         }
+//     }
+// `)
