665d38faa3b3dfbac9725fe754b78c72d2b0d313
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-07-09 00:05:31 +0900

    Add useGetRyotei

--- feature/ryotei/components/TimelineView.tsx
+++ feature/ryotei/components/TimelineView.tsx
@@ -3,40 +3,21 @@
 import { Timeline, BottomSheet, Layout, Form, Button, AddIcon, Plan, NoData } from '@/component'
 import { useTimeline } from '@/feature/ryotei/hooks/useTimeline'
 import { useMemo } from 'react'
-import { QUERY_GET_RYOTEI } from '@/feature/api/supabase'
-import { GetRyoteiQuery, GetRyoteiQueryVariables, RyoteiOrderBy } from '@/feature/api/graphql'
-import { useQuery } from '@apollo/client'
-import { format, parseISO } from 'date-fns'
+import { useGetRyotei } from './useGetRyotei'
 
 type Props = { data?: Record<string, Plan[]> }
 
-export const TimelineView = ({ data }: Props) => {
+export const TimelineView = () => {
   const containerStyle = 'flex flex-col justify-between p-10 max-w-xl m-auto container'
   const { handleClick, bottomSheet, setNewData, onMenuClick } = useTimeline()
+  const data = useGetRyotei()
   const isExist = useMemo(() => data && Object.keys(data).length > 0, [data])
-
-  const { data: a } = useQuery<GetRyoteiQuery, GetRyoteiQueryVariables>(QUERY_GET_RYOTEI, {
-    variables: { orderBy: 'datetime' as RyoteiOrderBy },
-  })
-  // console.log(a)
-  const res = a?.ryoteiCollection?.edges?.map((edge) => edge.node)
-  const grouped: Record<string, Plan[]> | undefined = res?.reduce(
-    (acc: Record<string, Plan[]>, { id, datetime, description }: Plan) => {
-      const date = format(parseISO(datetime), 'yyyy-MM-dd')
-      if (!acc[date]) {
-        acc[date] = []
-      }
-      acc[date].push({ datetime: format(parseISO(datetime), 'HH:mm'), description, id })
-      return acc
-    },
-    {}
-  )
 
   return (
     <>
       {isExist ? (
-        Object.entries(grouped || {}).map(([key, b]) => (
-          <Timeline key={key} title={key} items={b} onClick={onMenuClick} className="mb-3" />
+        Object.entries(data || {}).map(([key, item]) => (
+          <Timeline key={key} title={key} items={item} onClick={onMenuClick} className="mb-3" />
         ))
       ) : (
         <NoData />
---
+++ feature/ryotei/components/useGetRyotei.ts
@@ -0,0 +1,36 @@
+import { Plan } from '@/component'
+import { QUERY_GET_RYOTEI } from '@/feature/api/supabase'
+import { GetRyoteiQuery, GetRyoteiQueryVariables, RyoteiOrderBy, OrderByDirection } from '@/feature/api/graphql'
+import { useQuery } from '@apollo/client'
+import { format, parseISO } from 'date-fns'
+
+export const useGetRyotei = () => {
+  const variables: GetRyoteiQueryVariables = {
+    orderBy: [{ datetime: OrderByDirection.DescNullsLast }],
+  }
+  const { data } = useQuery<GetRyoteiQuery, GetRyoteiQueryVariables>(QUERY_GET_RYOTEI, {
+    variables,
+  })
+
+  const res = data?.ryoteiCollection?.edges?.map((edge) => edge.node)
+  const grouped: Record<string, Plan[]> | undefined = res?.reduce(
+    (acc: Record<string, Plan[]>, { id, datetime, description }: Plan) => {
+      const date = format(parseISO(datetime), 'yyyy-MM-dd')
+      if (!acc[date]) {
+        acc[date] = []
+      }
+      acc[date].push({ datetime: format(parseISO(datetime), 'HH:mm'), description, id })
+      acc[date].sort((a, b) => a.datetime.localeCompare(b.datetime))
+      return acc
+    },
+    {}
+  )
+  return grouped
+    ? Object.keys(grouped)
+        .sort()
+        .reduce((acc: Record<string, Plan[]>, key: string) => {
+          acc[key] = grouped[key]
+          return acc
+        }, {})
+    : undefined
+}
