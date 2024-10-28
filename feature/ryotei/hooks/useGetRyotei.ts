4abe31fe5d920203b1e3fcec5d81d69eebdeb93b
sjbworks <sacred.jizo.bosatz@gmail.com>
2024-10-29 00:27:44 +0900

    fix: fix useGetRyotei logic

--- feature/ryotei/hooks/useGetRyotei.ts
+++ feature/ryotei/hooks/useGetRyotei.ts
@@ -3,7 +3,9 @@ import { Plan } from '@/component'
 import { QUERY_GET_RYOTEI } from '@/feature/ryotei/graphql'
 import { GetRyoteiQuery, GetRyoteiQueryVariables, OrderByDirection } from '@/feature/api/graphql'
 import { useQuery } from '@apollo/client'
-import { format, parseISO } from 'date-fns'
+import { format } from 'date-fns'
+import { TZDate } from '@date-fns/tz'
+import { ja } from 'date-fns/locale/ja'
 
 export const useGetRyotei = () => {
   const variables: GetRyoteiQueryVariables = {
@@ -16,12 +18,14 @@ export const useGetRyotei = () => {
   const res = data?.ryoteiCollection?.edges?.map((edge) => edge.node)
   const grouped: Record<string, Plan[]> | undefined = res?.reduce(
     (acc: Record<string, Plan[]>, { id, datetime, description }: Plan) => {
-      const date = format(parseISO(datetime), 'yyyy-MM-dd')
-      if (!acc[date]) {
-        acc[date] = []
+      const rowTZDate = new TZDate(datetime + 'Z')
+      const formatDate = format(rowTZDate, 'yyyy-MM-dd', { locale: ja })
+      const formatRowDate = format(rowTZDate, "yyyy-MM-dd'T'HH:mm:ss.SS", { locale: ja })
+      if (!acc[formatDate]) {
+        acc[formatDate] = []
       }
-      acc[date].push({ datetime, description, id })
-      acc[date].sort((a, b) => a.datetime.localeCompare(b.datetime))
+      acc[formatDate].push({ datetime: formatRowDate, description, id })
+      acc[formatDate].sort((a, b) => a.datetime.localeCompare(b.datetime))
       return acc
     },
     {}
