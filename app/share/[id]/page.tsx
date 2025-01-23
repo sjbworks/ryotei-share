4634b5b4a16a9e20cea2c3811c17d0f89580a659
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-01-23 23:50:54 +0900

    commentout share page

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -1,15 +1,15 @@
-import { TimelineView } from '@/feature/ryotei/components/TimelineView'
-import { logout } from '@/feature/auth/api'
-import { Layout } from '@/component'
+// import { MainView } from '@/feature/ryotei/components/MainView'
+// import { logout } from '@/feature/auth/api'
+// import { Layout } from '@/component'
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const containerStyle = 'flex flex-col justify-between p-10 max-w-xl m-auto container'
+// export default async function Page({ params }: { params: { id: string } }) {
+//   const containerStyle = 'flex flex-col justify-between p-10 max-w-xl m-auto container'
 
-  return (
-    <main className={containerStyle}>
-      <Layout logout={logout}>
-        <TimelineView />
-      </Layout>
-    </main>
-  )
-}
+//   return (
+//     <main className={containerStyle}>
+//       <Layout logout={logout}>
+//         <MainView />
+//       </Layout>
+//     </main>
+//   )
+// }
