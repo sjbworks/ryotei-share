1af1bb37778172f173153ee31d6918dc9f2daa11
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-01-30 00:04:34 +0900

    Refactor Drawer component

--- component/Drawer/index.tsx
+++ component/Drawer/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 import SwipeableDrawer from '@mui/material/SwipeableDrawer'
 import { FC, ReactNode } from 'react'
-
+import { Global, type GlobalProps } from '@emotion/react';
 type Props = {
   open: boolean
   onClose: () => void
@@ -9,12 +9,16 @@ type Props = {
   anchor: 'bottom' | 'left'
   children: ReactNode
   className?: string
+  styles?: GlobalProps['styles']
 }
 export const Drawer: FC<Props> = (props) => {
-  const { open, onClose, onOpen, children, className } = props
+  const { open, onClose, onOpen, children, className, styles } = props
   return (
-    <SwipeableDrawer open={open} onClose={onClose} onOpen={onOpen} anchor="bottom" className={className}>
-      {children}
-    </SwipeableDrawer>
+    <>
+      {styles && <Global styles={styles} />}
+      <SwipeableDrawer open={open} onClose={onClose} onOpen={onOpen} anchor="bottom" className={className} >
+        {children}
+      </SwipeableDrawer>
+    </>
   )
 }
