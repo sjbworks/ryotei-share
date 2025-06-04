b8bbb7dcd12478a1cd107695d2911d4fedac748e
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-06-05 00:27:09 +0900

    lint fix

--- feature/ryotei/components/TimelineView.tsx
+++ feature/ryotei/components/TimelineView.tsx
@@ -5,21 +5,21 @@ import { Timeline, NoData } from '@/component'
 import { Typography } from '@mui/material'
 
 type Props = {
-    title: string;
+  title: string
 }
 
 export const TimelineView = ({ title }: Props) => {
-    const { data, refetch } = useGetRyotei()
-    const { onMenuClick, } = useTimeline(refetch)
-    const isExist = useMemo(() => data && Object.keys(data).length > 0, [data])
-    if (!isExist) return <NoData />
+  const { data, refetch } = useGetRyotei()
+  const { onMenuClick } = useTimeline(refetch)
+  const isExist = useMemo(() => data && Object.keys(data).length > 0, [data])
+  if (!isExist) return <NoData />
 
-    return (
-        <>
-            <Typography>{title}</Typography>
-            {Object.entries(data || {}).map(([key, item]) => (
-                <Timeline key={key} title={key} items={item} onClick={onMenuClick} className="mb-3" />
-            ))}
-        </>
-    )
+  return (
+    <>
+      <Typography>{title}</Typography>
+      {Object.entries(data || {}).map(([key, item]) => (
+        <Timeline key={key} title={key} items={item} onClick={onMenuClick} className="mb-3" />
+      ))}
+    </>
+  )
 }
