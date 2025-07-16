a525a43e97f45f779c7df7dfb1d4cc3195429113
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-07-16 19:45:18 +0900

    Adjust NoData styles

--- component/NoData/NoData.tsx
+++ component/NoData/NoData.tsx
@@ -1,13 +1,15 @@
 import { HikingIcon } from '@/component/Icon'
+import { Typography } from '@mui/material'
+import { grey } from '@mui/material/colors'
 
 export const NoData = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <div className="flex align-center">
-        <span className="mr-2">
-          <HikingIcon />
-        </span>
-        <p>旅程を登録してあなたの旅程表を作りましょう！</p>
+    <div className="flex flex-col items-center justify-center h-full background">
+      <div className="flex align-center items-center ">
+        <HikingIcon fontSize="large" sx={{ color: grey[500] }} />
+        <Typography color="grey.600" variant="body1">
+          旅程を登録してあなたの旅程表を作りましょう！
+        </Typography>
       </div>
     </div>
   )
