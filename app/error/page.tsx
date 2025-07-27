e29e200642b3ecf50d25ca5879db86cf36cadecb
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-07-28 00:22:33 +0900

    Add @cloudflare/next-on-pages

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = 'edge'
+
 export default function ErrorPage() {
   return <p>Sorry, something went wrong</p>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { MainView } from '@/feature/ryotei/components/MainView'
 
+export const runtime = 'edge'
+
 export default async function Home() {
   return <MainView />
 }
