3e06ffb3f825d5724dd116bdeefc5c17c96f7325
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-08-12 21:39:20 +0900

    fix redirectTo in feature/auth/api/login

--- feature/auth/api/login.ts
+++ feature/auth/api/login.ts
@@ -2,15 +2,24 @@ import { Provider } from '@supabase/auth-js'
 import { createClient } from '@/utils/supabase/client'
 
 export async function login(provider: Provider) {
+  const getURL = () => {
+    let url = process?.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/' // Automatically set by Vercel.
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.endsWith('/') ? url : `${url}/`
+    return url
+  }
+
   try {
     const supabase = createClient()
     const { error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: `${window.location.origin}/api/auth/callback`,
+        redirectTo: `${getURL()}/api/auth/callback`,
       },
     })
-    
+
     if (error) {
       console.error('Login error:', error)
       throw new Error(`Failed to login with ${provider}: ${error.message}`)
