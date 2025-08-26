c8d0e571c0ca32573328f75dc0a5a767f8185e9a
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-08-26 23:29:56 +0900

    Unnecessary code

--- next.config.js
+++ next.config.js
@@ -26,34 +26,6 @@ const nextConfig = {
 
     return config
   },
-  // Edge runtime compatibility
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.gql$/,
-  //     exclude: /node_modules/,
-  //     use: [
-  //       {
-  //         loader: 'graphql-tag/loader',
-  //       },
-  //     ],
-  //   })
-
-  //   // Edge runtime polyfills for Supabase
-  //   config.resolve.alias = {
-  //     ...config.resolve.alias,
-  //     'process/browser': require.resolve('process/browser'),
-  //   }
-
-  //   const webpack = require('webpack')
-  //   config.plugins.push(
-  //     new webpack.DefinePlugin({
-  //       'process.version': JSON.stringify('v18.0.0'),
-  //       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
-  //     })
-  //   )
-
-  //   return config
-  // },
 }
 
 module.exports = nextConfig
