66f904ee4667190c2dae5a38541f37239fac1f26
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-08-27 23:50:43 +0900

    Edit login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 import { AuthButton } from '@/feature/auth/components/AuthButton'
 import { login } from '@/feature/auth/api'
+import Image from 'next/image'
+import WalkingManSvg from '@/assets/svg/WalkingMan.svg'
 
 export default function Login() {
   const containerStyle = 'flex flex-col justify-center items-center h-screen gap-2'
 
   return (
     <main className={containerStyle}>
+      <Image src={WalkingManSvg} alt="Walking Man" width={150} height={150} />
       <AuthButton login={login} />
     </main>
   )
