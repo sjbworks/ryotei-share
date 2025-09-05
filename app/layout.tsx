b068f9f458f531fdf8c8ff93db487725af209a0a
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-09-05 21:15:08 +0900

    Add metadata to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,35 @@ import { ApolloProvider } from '@/feature/provider/ApolloProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'Ryotei Share',
+  description: '旅程を作成するためのWebサービスです。',
+  keywords: ['旅行', '旅のしおり', '行程表', '旅程', '旅程表'],
+  authors: [{ name: 'Ryotei Share' }],
+  icons: {
+    icon: '/favicon.jpg?v=1',
+  },
+  openGraph: {
+    title: 'Ryotei Share',
+    description: '旅程を作成するためのWebサービスです。',
+    type: 'website',
+    locale: 'ja_JP',
+  },
+  twitter: {
+    card: 'summary',
+    title: 'Ryotei Share',
+    description: '旅程を作成するためのWebサービスです。',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const className = clsx(inter.className, 'flex flex-col')
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={className}>
         <ApolloProvider>{children}</ApolloProvider>
       </body>
