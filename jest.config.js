9914fdcc31c7d4595ecabee0f93232392f373bde
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-09-06 20:22:47 +0900

    feat: set up Jest testing framework

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  coverageProvider: 'v8',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: [
+    'component/**/*.{ts,tsx}',
+    'feature/**/*.{ts,tsx}',
+    'app/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/*.stories.{ts,tsx}',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
---
+++ jest.setup.js
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom'
+
+// Mock Next.js Image component
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text, @next/next/no-img-element
+    return <img {...props} />
+  },
+}))
+
+// Mock next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    back: jest.fn(),
+  }),
+  usePathname: () => '/',
+}))
+
+// Mock SVG imports
+jest.mock('@/assets/svg/WalkingMan.svg', () => ({
+  __esModule: true,
+  default: '/mock-walking-man.svg',
+}))
