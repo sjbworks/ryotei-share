a111fb457ad9348236cc5b6340ac449d25fb8ba5
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-09-16 00:40:24 +0900

    Fix useShareSettingCRUD

--- feature/ryotei/hooks/useShareSettingCRUD.ts
+++ feature/ryotei/hooks/useShareSettingCRUD.ts
@@ -2,24 +2,22 @@ import { useMutation } from '@apollo/client'
 import { useRouter } from 'next/navigation'
 import { MUTATION_ADD_SHARE_SETTING } from '@/feature/ryotei/graphql'
 import {
-  InsertIntoshare_SettingCollectionMutation as AddShareSettingMutation,
-  InsertIntoshare_SettingCollectionMutationVariables as AddShareSettingMutationVariables,
-  Share_SettingInsertInput,
+  InsertIntoshareCollectionMutation as AddShareMutation,
+  InsertIntoshareCollectionMutationVariables as AddShareMutationVariables,
+  ShareInsertInput,
 } from '@/feature/api/graphql'
 
 export const useShareSettingCRUD = () => {
-  const [addShareSetting, { data }] = useMutation<AddShareSettingMutation, AddShareSettingMutationVariables>(
-    MUTATION_ADD_SHARE_SETTING
-  )
+  const [addShareSetting] = useMutation<AddShareMutation, AddShareMutationVariables>(MUTATION_ADD_SHARE_SETTING)
 
-  const shareTrip = async (newData: Share_SettingInsertInput) => {
+  const shareTrip = async (newData: ShareInsertInput) => {
     try {
-      const insertData: Share_SettingInsertInput = {
+      const insertData: ShareInsertInput = {
         ...newData,
         trip_id: newData.trip_id,
       }
-      await addShareSetting({ variables: { objects: insertData } })
-      const shareId = await data?.insertIntoshare_settingCollection?.records[0].share_id
+      const result = await addShareSetting({ variables: { objects: insertData } })
+      const shareId = result.data?.insertIntoshareCollection?.records[0].share_id
       window.open(`/${shareId}`, '_blank', 'noopener,noreferrer')
     } catch (e) {
       /* Error Handling */
