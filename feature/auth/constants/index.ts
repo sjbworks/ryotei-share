f67bc65589a769b0ec285dbc51e4707b16f7209b
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-10-05 19:16:04 +0900

    chore: Change order of login providers

--- feature/auth/constants/index.ts
+++ feature/auth/constants/index.ts
@@ -1,6 +1,6 @@
 export const PROVIDER_LABELS = {
-  github: 'Github',
   google: 'Google',
+  github: 'Github',
 } as const
 
 export const PROVIDERS = Object.keys(PROVIDER_LABELS) as (keyof typeof PROVIDER_LABELS)[]
