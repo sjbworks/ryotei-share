b27de4e03efe158ecb8fadd406b0214b06675118
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-12-04 23:04:04 +0900

    Memorize formattedData in useGetRyotei

--- feature/ryotei/hooks/useGetRyotei.ts
+++ feature/ryotei/hooks/useGetRyotei.ts
@@ -3,6 +3,7 @@ import { QUERY_GET_RYOTEI } from '@/feature/ryotei/graphql'
 import { GetRyoteiQuery, GetRyoteiQueryVariables, OrderByDirection } from '@/feature/api/graphql'
 import { useQuery } from '@apollo/client/react'
 import { formatRyoteiData } from '@/feature/ryotei/utils/formatRyoteiData'
+import { useMemo } from 'react'
 
 export const useGetRyotei = (selectedTripId?: string) => {
   const variables: GetRyoteiQueryVariables = {
@@ -15,7 +16,7 @@ export const useGetRyotei = (selectedTripId?: string) => {
   })
 
   const nodes = data?.ryoteiCollection?.edges?.map((edge) => edge.node)
-  const formattedData = formatRyoteiData(nodes)
+  const formattedData = useMemo(() => formatRyoteiData(nodes), [nodes])
 
   return {
     data: formattedData,
