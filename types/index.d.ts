329400e525cefcd7d7f7309f0df7b3b2f692c61f
sjbworks <sacred.jizo.bosatz@gmail.com>
2026-03-10 22:29:17 +0900

    Add type declaretion to types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,3 +4,18 @@ declare module '*.gql' {
   const value: DocumentNode
   export = value
 }
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
