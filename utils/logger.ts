8056a9cbf3aaa9590b86d6af25d38534133427ba
sjbworks <sacred.jizo.bosatz@gmail.com>
2026-06-26 16:12:34 +0900

    Add a condition for sending log to logger

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,5 +1,6 @@
 export const logError = (message: string, context?: Record<string, unknown>) => {
   console.error(message, context)
+  if (process.env.NODE_ENV !== 'production') return
   fetch('/api/log', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
