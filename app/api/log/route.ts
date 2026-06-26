11fdbbdf47b161903d3e14bb51ac1cb2a1c8f88a
sjbworks <sacred.jizo.bosatz@gmail.com>
2026-06-26 15:34:43 +0900

    Add authentification to log endpoint

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -1,6 +1,15 @@
 import { NextResponse, NextRequest } from 'next/server'
+import { createClientForServer } from '@/utils/supabase/server'
 
 export async function POST(req: NextRequest) {
+  const supabase = await createClientForServer()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  if (!user) {
+    return NextResponse.json({ ok: false }, { status: 401 })
+  }
+
   try {
     const body = await req.json()
     const { message, context } = body as { message: string; context?: Record<string, unknown> }
